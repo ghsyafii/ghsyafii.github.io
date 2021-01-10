bb207148a0d260c48a8c9b2906b7b4534c037c79
ghsyafii <73457694+ghsyafii@users.noreply.github.com>
2021-01-10 13:16:24 +0800

    popup test

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
-//Hey!
+const popupWrapper = document.querySelector('.popup-wrapper')
+
+const button = document.querySelector('button');
+
+const close = document.querySelector('.close');
+
+button.addEventListener('click', () => {
+    popupWrapper.style.display = 'initial';
+});
+
+close.addEventListener('click', ()=>{
+    popupWrapper.style.display = 'none';
+})
