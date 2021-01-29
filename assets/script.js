2256f41e7e399a93ccc6a9426621d2e817c376f4
ghsyafii <73457694+ghsyafii@users.noreply.github.com>
2021-01-29 23:42:34 +0800

    added colour changer

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
-console.log("Heyyyyy");
+const changer = document.querySelector('div.colour-changer input');
+const bodyTag = document.querySelector('body');
+
+changer.addEventListener('input', () => {
+    bodyTag.style.backgroundColor = changer.value;
+
+    const color = chroma(changer.value);
+
+    if (color.luminance()<0.3){
+        bodyTag.classList.add("dark");
+    }
+    else {
+        bodyTag.classList.remove("dark");
+    }
+});
